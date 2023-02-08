a19eade90466e29d64cbf7f1c8233a64a0852c34
Samson063 <furthermaths078@gamil.com>
2023-02-08 23:09:17 +0100

    ressolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React from "react";
-import "./App.scss";
-import Particles from "react-tsparticles";
+import React from 'react'
+import './App.scss'
+import Particles from 'react-tsparticles'
 // import { Landing } from "./Pages";
-import { loadStarsPreset } from "tsparticles-preset-stars";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
-import Explore from "./Pages/explore";
+import { loadStarsPreset } from 'tsparticles-preset-stars'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Explore from './Pages/explore'
+import Repos from './Pages/Repositories/Repos'
 
 const App = () => {
   const options = {
-    preset: "stars",
+    preset: 'stars',
     Animation: {
       enable: true,
       speed: 30,
       minimumValue: 0.1,
       sync: false,
     },
-  };
+  }
 
   const initialize = (instance) => {
-    loadStarsPreset(instance);
-  };
+    loadStarsPreset(instance)
+  }
 
   return (
-    <div className="App">
-      <div className="components-containter">
+    <div className='App'>
+      <div className='components-containter'>
         <ErrorBoundary>
           {/* <Routing /> */}
           <Explore />
           {/* <Landing /> */}
+          <Repos />
         </ErrorBoundary>
       </div>
 
-      <div className="particles-container">
-        <Particles id="tsparticles" options={options} init={initialize} />
+      <div className='particles-container'>
+        <Particles id='tsparticles' options={options} init={initialize} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
