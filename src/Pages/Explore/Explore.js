6572ca13194c9c4a27b297774b00c841a45fe384
Samson063 <furthermaths078@gamil.com>
2023-02-13 13:15:33 +0100

    Added the explore search button

--- src/Pages/Explore/Explore.js
+++ src/Pages/Explore/Explore.js
@@ -9,7 +9,9 @@ import { Gi3DGlasses } from 'react-icons/gi'
 const Explore = () => {
   const [repositories, setRepositories] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  const githubURL = 'https://api.github.com/users/samson063/repos'
+  const [organization, setOrganization] = useState('facebook')
+
+  const githubURL = `https://api.github.com/orgs/${organization}/repos`
 
   useEffect(() => {
     setIsLoading(true)
@@ -24,10 +26,30 @@ const Explore = () => {
       })
   }, [])
 
+  const handleSearch = () => {
+    setIsLoading(true)
+    axios
+      .get(githubURL)
+      .then((res) => {
+        setIsLoading(false)
+        setRepositories(res.data)
+      })
+      .catch((err) => {
+        // alert('no result')
+        setIsLoading(false)
+        setRepositories([])
+        console.log(err.message)
+      })
+  }
+
   return (
     <>
       <Navbar />
       {isLoading && <p className='loading'>Loading...</p>}
+      {!isLoading && repositories.length < 1 && (
+        <p className='loading'>No result found</p>
+      )}
+
       {!isLoading && (
         <>
           <div className='explore'>
@@ -37,6 +59,16 @@ const Explore = () => {
                 <Gi3DGlasses />
               </i>
             </h1>
+            <div>
+              <input
+                className='search'
+                placeholder='Search Organisation'
+                onChange={(val) => setOrganization(val.target.value)}
+              />
+              <button className='search-btn' onClick={handleSearch}>
+                Search
+              </button>
+            </div>
           </div>
           <div className='repo-box'>
             {repositories.map((repo) => {
